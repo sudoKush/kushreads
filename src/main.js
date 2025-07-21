4ac5a29c23bdff4f281281b4ab743595486c8d24
sudoKush <404kushfound@gmail.com>
2025-07-22 01:03:36 +0530

    move: moved to ./src/scripts/

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import '../styles/general.css'
-import '../styles/header.css'
-import '../styles/hero.css'
-import '../styles/sections.css'
-import '../styles/footer.css'
-import '../styles/med-q.css'
-
