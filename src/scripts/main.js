bc2ecd7339de46da309c266f52ff68572c9e43fa
sudoKush <404kushfound@gmail.com>
2025-07-22 01:49:52 +0530

    remove: file removed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,6 @@
 import '../../styles/general.css'
 import '../../styles/header.css'
 import '../../styles/main.css'
-import '../../styles/sections.css'
 import '../../styles/footer.css'
 import '../../styles/med-q.css'
 
