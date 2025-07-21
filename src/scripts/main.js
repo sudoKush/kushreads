a7a59aa39e91749704f1194d02c62c3457a7c483
sudoKush <404kushfound@gmail.com>
2025-07-22 01:30:43 +0530

    add/fix: new logo for the web(logo.png) and name fix during importing (main.js)

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+import '../../styles/general.css'
+import '../../styles/header.css'
+import '../../styles/main.css'
+import '../../styles/sections.css'
+import '../../styles/footer.css'
+import '../../styles/med-q.css'
+
